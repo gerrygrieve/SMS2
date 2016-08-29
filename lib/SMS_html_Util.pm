352b45195be8b8faeb38d83f8f219b6277f56166
gerry grieve <grieve@phas.ubc.ca>
2016-08-29 12:20:29 -0700

    add availibility table in lib/SMS_html_Util.pm

--- lib/SMS_html_Util.pm
+++ lib/SMS_html_Util.pm
@@ -60,63 +60,96 @@ sub student_register {
  
 		$out .= qq{<td class="input"> $xout</td></tr>\n};
 	}
+	$out .= mk_avail_table($q);
 
-	$out .=  qq{ <tr><td class="explain" colspan="2"> Below is the list of upcoming courses.
- 
-    You can check the
-	<a href="JavaScript:newPopup('http://gamma.phas.ubc.ca/cgi-bin/SMS2/mkcal');">SMS Calendar</a> for available dates.
-              </td></tr>};
- 
-	my $data = course_metadata::get_Course_Data_json();
-	my @courses = @{$data->{course_data}};
-	my $ctable = "";
-	my $headers = qq{<tr>};
-	$headers   .= qq{<th class="ct">Modules</th>};
-	$headers   .= qq{<th class="ct">Title</th>};
-	$headers   .= qq{<th class="ct">Date</th>};
-	$headers   .= qq{<th class="ct">Request</th>};
-	$headers   .= qq{<th class="ct">Registered</th>};
-	$headers   .= qq{<th class="ct">Completed</th>};
-	$headers   .= qq{</tr>};
-	my $modnum;
-	foreach my $c (  @courses ) {
-		$modnum = $c->{num};
-		my $title  = $c->{title};
-		$ctable .= qq{  <tr><td class="ct"> Module # $modnum</td>};
-		$ctable .= qq{      <td  class="ct"> $title</td>};
-		my @dates = @{$c->{date_upcoming}};
-		my $dtable = qq{<table class="dtable"><tr>};
-		$ctable .= qq{  <td  class="ct">};
-		foreach my $date ( @dates ) {
-			next if $date < $isotoday;
-			$ctable .= qq{  $date<br />};
-		}
-		$ctable .= qq{</td> <td  class="ct"> };
-		foreach my $date ( @dates ) {
-			next if $date < $isotoday;
-			my $but = "$modnum.ask.$date";
-            
-			my $xbut = $q->checkbox(-name=>$but,
-			                        -checked=>0,
-									-value=>'ON',
-									-label=>' ');
-			$ctable .= qq{  $xbut<br />};
-		}
-		$ctable .= qq{</td> <td  class="ct"> };
-		foreach my $date ( @dates ) {
-			next if $date < $isotoday;
-			$ctable .= qq{     --<br />};
-		}
-		$ctable .= qq{</td> <td  class="ct"> };
-		foreach my $date ( @dates ) {
-			next if $date < $isotoday;	
-			$ctable .= qq{  --<br />};
-		}
-		$ctable .= qq{</td> };
-		$ctable .= qq{</tr>\n};
-    }
-	$out .= qq{<table id="ctable">$headers $ctable</table>};     
+#	$out .=  qq{ <tr><td class="explain" colspan="2"> Below is the list of upcoming courses.
+# 
+#    You can check the
+#	<a href="JavaScript:newPopup('http://gamma.phas.ubc.ca/cgi-bin/SMS2/mkcal');">SMS Calendar</a> for available dates.
+#              </td></tr>};
+# 
+#	my $data = course_metadata::get_Course_Data_json();
+#	my @courses = @{$data->{course_data}};
+#	my $ctable = "";
+#	my $headers = qq{<tr>};
+#	$headers   .= qq{<th class="ct">Modules</th>};
+#	$headers   .= qq{<th class="ct">Title</th>};
+#	$headers   .= qq{<th class="ct">Date</th>};
+#	$headers   .= qq{<th class="ct">Request</th>};
+#	$headers   .= qq{<th class="ct">Registered</th>};
+#	$headers   .= qq{<th class="ct">Completed</th>};
+#	$headers   .= qq{</tr>};
+#	my $modnum;
+#	foreach my $c (  @courses ) {
+#		$modnum = $c->{num};
+#		my $title  = $c->{title};
+#		$ctable .= qq{  <tr><td class="ct"> Module # $modnum</td>};
+#		$ctable .= qq{      <td  class="ct"> $title</td>};
+#		my @dates = @{$c->{date_upcoming}};
+#		my $dtable = qq{<table class="dtable"><tr>};
+#		$ctable .= qq{  <td  class="ct">};
+#		foreach my $date ( @dates ) {
+#			next if $date < $isotoday;
+#			$ctable .= qq{  $date<br />};
+#		}
+#		$ctable .= qq{</td> <td  class="ct"> };
+#		foreach my $date ( @dates ) {
+#			next if $date < $isotoday;
+#			my $but = "$modnum.ask.$date";
+#            
+#			my $xbut = $q->checkbox(-name=>$but,
+#			                        -checked=>0,
+#									-value=>'ON',
+#									-label=>' ');
+#			$ctable .= qq{  $xbut<br />};
+#		}
+#		$ctable .= qq{</td> <td  class="ct"> };
+#		foreach my $date ( @dates ) {
+#			next if $date < $isotoday;
+#			$ctable .= qq{     --<br />};
+#		}
+#		$ctable .= qq{</td> <td  class="ct"> };
+#		foreach my $date ( @dates ) {
+#			next if $date < $isotoday;	
+#			$ctable .= qq{  --<br />};
+#		}
+#		$ctable .= qq{</td> };
+#		$ctable .= qq{</tr>\n};
+#    }
+#	$out .= qq{<table id="ctable">$headers $ctable</table>};     
 
 	return $out;
 
 }
+
+sub mk_avail_table {
+
+	my $q = shift;
+	my $out = "";
+
+	$out .= qq{<table id="avail">};
+	$out .= qq{<tr class="avail_head">};
+	$out .= qq{   <th class="avail_head"> &nbsp; </th>};
+	$out .= qq{   <th class="avail_head"> Mon </th>};
+	$out .= qq{   <th class="avail_head"> Tue </th>};
+	$out .= qq{   <th class="avail_head"> Wed </th>};
+	$out .= qq{   <th class="avail_head"> Thr </th>};
+	$out .= qq{   <th class="avail_head"> Fri </th>};
+	$out .= qq{   </tr>};
+
+	$out .= qq{<tr class="avail">};
+	foreach my $time ( "AM", "PM" ){
+		$out .= qq{   <td class="avail"> $time </td>};
+		foreach $day ( qw[ Mon Tue Wed Thr Fri] ) {
+			my $butname = $day . "_" . $time;
+			my $but = $q->checkbox(-name=>$butname,
+			                       -checked=>1,
+			                       -value=>'ON',
+			                       -label=>'Available ');
+			$out .= qq{   <td class="avail"> $but</td>};
+		}
+		$out .= qq{   </tr>};
+	}
+	$out .= qq{   </table>};
+}
+	
